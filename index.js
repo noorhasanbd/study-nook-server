f293a1f2be2d38a68e4351ffb8f672cff826918d
noorhasanbd <noor.hasan982845@gmail.com>
2026-05-20 11:51:42 +0600

    Connection with MongoDB server is done

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-const dns = require('dns');
-dns.setServers(['8.8.8.8', "8.8.4.4"]);
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-const uri= process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
